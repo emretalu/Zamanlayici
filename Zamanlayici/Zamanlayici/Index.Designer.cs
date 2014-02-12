6f039acdfb0ebf15fca4559d1fe2d45edd2da0e1
emretalu <emretalu87@gmail.com>
2014-02-13 00:12:27 +0200

    upload all files

---
+++ Zamanlayici/Zamanlayici/Index.Designer.cs
@@ -0,0 +1,371 @@
+﻿namespace Zamanlayici
+{
+    partial class Index
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Index));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.dosyaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.çıkışToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.hakkımızdaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.lblStatus = new System.Windows.Forms.ToolStripStatusLabel();
+            this.radioShutdown = new System.Windows.Forms.RadioButton();
+            this.radioLogoff = new System.Windows.Forms.RadioButton();
+            this.radioRestart = new System.Windows.Forms.RadioButton();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblTime = new System.Windows.Forms.Label();
+            this.lblNotif = new System.Windows.Forms.Label();
+            this.lblHour = new System.Windows.Forms.Label();
+            this.lblMinute = new System.Windows.Forms.Label();
+            this.cmbHour = new System.Windows.Forms.ComboBox();
+            this.cmbMinute = new System.Windows.Forms.ComboBox();
+            this.btnSet = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.menuStrip1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.dosyaToolStripMenuItem,
+            this.hakkımızdaToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(284, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // dosyaToolStripMenuItem
+            // 
+            this.dosyaToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.çıkışToolStripMenuItem});
+            this.dosyaToolStripMenuItem.Name = "dosyaToolStripMenuItem";
+            this.dosyaToolStripMenuItem.Size = new System.Drawing.Size(51, 20);
+            this.dosyaToolStripMenuItem.Text = "Dosya";
+            // 
+            // çıkışToolStripMenuItem
+            // 
+            this.çıkışToolStripMenuItem.Name = "çıkışToolStripMenuItem";
+            this.çıkışToolStripMenuItem.Size = new System.Drawing.Size(99, 22);
+            this.çıkışToolStripMenuItem.Text = "Çıkış";
+            this.çıkışToolStripMenuItem.Click += new System.EventHandler(this.çıkışToolStripMenuItem_Click);
+            // 
+            // hakkımızdaToolStripMenuItem
+            // 
+            this.hakkımızdaToolStripMenuItem.Name = "hakkımızdaToolStripMenuItem";
+            this.hakkımızdaToolStripMenuItem.Size = new System.Drawing.Size(81, 20);
+            this.hakkımızdaToolStripMenuItem.Text = "Hakkımızda";
+            this.hakkımızdaToolStripMenuItem.Click += new System.EventHandler(this.hakkımızdaToolStripMenuItem_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.lblStatus});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 240);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(284, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(43, 17);
+            this.lblStatus.Text = "Hazır...";
+            // 
+            // radioShutdown
+            // 
+            this.radioShutdown.AutoSize = true;
+            this.radioShutdown.Checked = true;
+            this.radioShutdown.Location = new System.Drawing.Point(19, 39);
+            this.radioShutdown.Name = "radioShutdown";
+            this.radioShutdown.Size = new System.Drawing.Size(102, 17);
+            this.radioShutdown.TabIndex = 2;
+            this.radioShutdown.TabStop = true;
+            this.radioShutdown.Text = "Bilgisayarı Kapat";
+            this.radioShutdown.UseVisualStyleBackColor = true;
+            // 
+            // radioLogoff
+            // 
+            this.radioLogoff.AutoSize = true;
+            this.radioLogoff.Location = new System.Drawing.Point(19, 62);
+            this.radioLogoff.Name = "radioLogoff";
+            this.radioLogoff.Size = new System.Drawing.Size(96, 17);
+            this.radioLogoff.TabIndex = 3;
+            this.radioLogoff.Text = "Oturumu Kapat";
+            this.radioLogoff.UseVisualStyleBackColor = true;
+            // 
+            // radioRestart
+            // 
+            this.radioRestart.AutoSize = true;
+            this.radioRestart.Location = new System.Drawing.Point(19, 85);
+            this.radioRestart.Name = "radioRestart";
+            this.radioRestart.Size = new System.Drawing.Size(96, 17);
+            this.radioRestart.TabIndex = 4;
+            this.radioRestart.Text = "Yeniden Başlat";
+            this.radioRestart.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Controls.Add(this.lblTime);
+            this.panel1.Controls.Add(this.lblNotif);
+            this.panel1.Location = new System.Drawing.Point(144, 39);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(128, 63);
+            this.panel1.TabIndex = 5;
+            // 
+            // lblTime
+            // 
+            this.lblTime.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.lblTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblTime.Location = new System.Drawing.Point(0, 41);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(124, 18);
+            this.lblTime.TabIndex = 7;
+            this.lblTime.Text = "00:00";
+            this.lblTime.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblNotif
+            // 
+            this.lblNotif.AutoSize = true;
+            this.lblNotif.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblNotif.Location = new System.Drawing.Point(-2, 0);
+            this.lblNotif.Name = "lblNotif";
+            this.lblNotif.Size = new System.Drawing.Size(128, 25);
+            this.lblNotif.TabIndex = 6;
+            this.lblNotif.Text = "Kalan Süre";
+            // 
+            // lblHour
+            // 
+            this.lblHour.AutoSize = true;
+            this.lblHour.Location = new System.Drawing.Point(33, 155);
+            this.lblHour.Name = "lblHour";
+            this.lblHour.Size = new System.Drawing.Size(38, 13);
+            this.lblHour.TabIndex = 7;
+            this.lblHour.Text = "Saat - ";
+            // 
+            // lblMinute
+            // 
+            this.lblMinute.AutoSize = true;
+            this.lblMinute.Location = new System.Drawing.Point(21, 180);
+            this.lblMinute.Name = "lblMinute";
+            this.lblMinute.Size = new System.Drawing.Size(50, 13);
+            this.lblMinute.TabIndex = 8;
+            this.lblMinute.Text = "Dakika - ";
+            // 
+            // cmbHour
+            // 
+            this.cmbHour.FormattingEnabled = true;
+            this.cmbHour.Items.AddRange(new object[] {
+            "0",
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10",
+            "11",
+            "12",
+            "13",
+            "14",
+            "15",
+            "16",
+            "17",
+            "18",
+            "19",
+            "20",
+            "21",
+            "22",
+            "23"});
+            this.cmbHour.Location = new System.Drawing.Point(77, 152);
+            this.cmbHour.Name = "cmbHour";
+            this.cmbHour.Size = new System.Drawing.Size(35, 21);
+            this.cmbHour.TabIndex = 10;
+            this.cmbHour.Text = "0";
+            // 
+            // cmbMinute
+            // 
+            this.cmbMinute.FormattingEnabled = true;
+            this.cmbMinute.Items.AddRange(new object[] {
+            "0",
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10",
+            "11",
+            "12",
+            "13",
+            "14",
+            "15",
+            "16",
+            "17",
+            "18",
+            "19",
+            "20",
+            "21",
+            "22",
+            "23",
+            "24",
+            "25",
+            "26",
+            "27",
+            "28",
+            "29",
+            "30",
+            "31",
+            "32",
+            "33",
+            "34",
+            "35",
+            "36",
+            "37",
+            "38",
+            "39",
+            "40",
+            "41",
+            "42",
+            "43",
+            "44",
+            "45",
+            "46",
+            "47",
+            "48",
+            "49",
+            "50",
+            "51",
+            "52",
+            "53",
+            "54",
+            "55",
+            "56",
+            "57",
+            "58",
+            "59"});
+            this.cmbMinute.Location = new System.Drawing.Point(77, 177);
+            this.cmbMinute.Name = "cmbMinute";
+            this.cmbMinute.Size = new System.Drawing.Size(35, 21);
+            this.cmbMinute.TabIndex = 11;
+            this.cmbMinute.Text = "0";
+            // 
+            // btnSet
+            // 
+            this.btnSet.Location = new System.Drawing.Point(163, 136);
+            this.btnSet.Name = "btnSet";
+            this.btnSet.Size = new System.Drawing.Size(86, 32);
+            this.btnSet.TabIndex = 12;
+            this.btnSet.Text = "KUR";
+            this.btnSet.UseVisualStyleBackColor = true;
+            this.btnSet.Click += new System.EventHandler(this.btnSet_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(163, 174);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(86, 34);
+            this.btnCancel.TabIndex = 13;
+            this.btnCancel.Text = "İptal Et";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Index
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnSet);
+            this.Controls.Add(this.cmbMinute);
+            this.Controls.Add(this.cmbHour);
+            this.Controls.Add(this.lblMinute);
+            this.Controls.Add(this.lblHour);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.radioRestart);
+            this.Controls.Add(this.radioLogoff);
+            this.Controls.Add(this.radioShutdown);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Index";
+            this.Text = "Zamanlayıcı";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem dosyaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem çıkışToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem hakkımızdaToolStripMenuItem;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel lblStatus;
+        private System.Windows.Forms.RadioButton radioShutdown;
+        private System.Windows.Forms.RadioButton radioLogoff;
+        private System.Windows.Forms.RadioButton radioRestart;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblTime;
+        private System.Windows.Forms.Label lblNotif;
+        private System.Windows.Forms.Label lblHour;
+        private System.Windows.Forms.Label lblMinute;
+        private System.Windows.Forms.ComboBox cmbHour;
+        private System.Windows.Forms.ComboBox cmbMinute;
+        private System.Windows.Forms.Button btnSet;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
---
+++ Zamanlayici/Zamanlayici/Index.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Zamanlayici
+{
+    public partial class Index : Form
+    {
+        int SAAT = 60 * 60;
+        int DAKIKA = 60;
+        int hour, minute, total;
+        string mesaj, argument;
+
+        public Index()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSet_Click(object sender, EventArgs e)
+        {
+            hour = Convert.ToInt32(cmbHour.SelectedIndex);
+            minute = Convert.ToInt32(cmbMinute.SelectedIndex);
+            total = (DAKIKA * minute) + (SAAT * hour);
+            if ((hour == 0 || hour == -1) && (minute == 0 || minute == -1))
+            {
+                MessageBox.Show("Bu işlemi gerçekleştiremezsiniz.");
+            }
+            else
+            {
+                if (radioShutdown.Checked)
+                {
+                    argument = "-s -f";
+                    mesaj = "Bilgisayar kapatma ayarlandı.";
+                }
+                else if (radioLogoff.Checked)
+                {
+                    argument = "-l -f";
+                    mesaj = "Oturum kapatma ayarlandı.";
+                }
+                else if (radioRestart.Checked)
+                {
+                    argument = "-r -f";
+                    mesaj = "Yeniden başlatma ayarlandı.";
+                }
+                lblStatus.Text = mesaj;
+                timer1.Start();
+                btnSet.Enabled = false;
+                btnCancel.Enabled = true;
+            }
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                timer1.Stop();
+                lblTime.Text = "00:00";
+                lblStatus.Text = "İşleminiz iptal edildi";
+                btnSet.Enabled = true;
+                btnCancel.Enabled = false;
+            }
+            catch (Exception genelHata)
+            {
+                MessageBox.Show("Hata Oluştu.\n" + genelHata.Message);
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            int hour_now, minute_now, second_now;
+            hour_now = (total) / SAAT;
+            minute_now = (total - (hour_now * SAAT)) / DAKIKA;
+            second_now = (total - (hour_now * SAAT) - (minute_now * DAKIKA));
+
+            if (total < 300)
+            {
+                lblTime.ForeColor = Color.DarkRed;
+            }
+            else
+            {
+                lblTime.ForeColor = Color.Black;
+            }
+
+            lblTime.Text = hour_now.ToString("D2") + ":" + minute_now.ToString("D2") + ":" + second_now.ToString("D2");
+            total--;
+            if (total == 0)
+            {
+                timer1.Stop();
+                try
+                {
+                    System.Diagnostics.Process.Start("shutdown", argument);
+                }
+                catch (Exception genelHata)
+                {
+                    MessageBox.Show("Hata Oluştu.\n" + genelHata.Message);
+                }
+            }
+        }
+
+        private void çıkışToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void hakkımızdaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            About abt = new About();
+            abt.Show();
+        }
+    }
+}
